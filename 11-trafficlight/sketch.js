207a605a2813b1e53db36f43a6b1e511138eea81
Dan Schellenberg <schellenberg@gmail.com>
2019-03-04 15:35:50 -0600

    added traffic light starter code

---
+++ 11-trafficlight/sketch.js
@@ -0,0 +1,29 @@
+// Traffic Light Starter Code
+// Dan Schellenberg
+// Sept 25, 2018
+
+// GOAL: make a 'traffic light' simulator. For now, just have the light
+// changing according to time. You may want to investigate the millis()
+// function at https://p5js.org/reference/
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(255);
+  drawOutlineOfLights();
+}
+
+function drawOutlineOfLights() {
+  //box
+  rectMode(CENTER);
+  fill(0);
+  rect(width/2, height/2, 75, 200, 10);
+
+  //lights
+  fill(255);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+  ellipse(width/2, height/2, 50, 50); //middle
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+}
