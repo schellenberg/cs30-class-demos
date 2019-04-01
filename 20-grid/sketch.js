e0d0988a407e695d9444cc52d8720a62185d3986
Dan Schellenberg <schellenberg@gmail.com>
2019-04-01 15:17:09 -0600

    grid demo

---
+++ 20-grid/sketch.js
@@ -0,0 +1,60 @@
+// Grid Demo
+// Dan Schellenberg
+// April 1, 2019
+
+let gridSize = 10;
+let grid;
+let cellSize;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = createRandom2DArray(gridSize, gridSize);
+  cellSize = width/gridSize;
+}
+
+function draw() {
+  background(255);
+  displayGrid();
+}
+
+function displayGrid() {
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill(255);
+      }
+      else {
+        fill(0);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+
+function create2DArray(cols, rows) {
+  let emptyArray = [];
+  for (let i = 0; i < rows; i++) {
+    emptyArray.push([]);
+    for (let j = 0; j < cols; j++) {
+      emptyArray[i].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function createRandom2DArray(cols, rows) {
+  let emptyArray = [];
+  for (let i = 0; i < rows; i++) {
+    emptyArray.push([]);
+    for (let j = 0; j < cols; j++) {
+      if (random(100) < 50) {
+        emptyArray[i].push(0);
+      }
+      else {
+        emptyArray[i].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
