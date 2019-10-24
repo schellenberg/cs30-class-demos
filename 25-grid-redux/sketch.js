d14f8877378e0857e1b2fcb4ee093332558d1625
Dan Schellenberg <schellenberg@gmail.com>
2019-10-24 13:56:35 -0600

    another 2d grid demo

---
+++ 25-grid-redux/sketch.js
@@ -0,0 +1,58 @@
+let grid;
+let rows = 30;
+let cols = 30;
+
+function setup() {
+  createCanvas(800, 800);
+  grid = createRandom2dArray(cols, rows);
+}
+
+function draw() {
+  background(220);
+  displayGrid(grid, rows, cols);
+}
+
+function mousePressed() {
+  let cellSize = width/cols;
+
+  let xCoord = floor(mouseX / cellSize);
+  let yCoord = floor(mouseY / cellSize);
+  
+  if (grid[yCoord][xCoord] === 1) {
+    grid[yCoord][xCoord] = 0;
+  }
+  else {
+    grid[yCoord][xCoord] = 1;
+  }
+}
+
+function displayGrid(grid, rows, cols) {
+  let cellSize = width / cols;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(1);
+      }
+      else {
+        randomGrid[x].push(0);
+      }
+    }
+  }
+  return randomGrid;
+}
