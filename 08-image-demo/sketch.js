e1a08b9428d17f4ac7df3b54f43a6d0853205fbf
Dan Schellenberg <schellenberg@gmail.com>
2020-02-12 15:13:18 -0600

    mouseWheel demo

--- 08-image-demo/sketch.js
+++ 08-image-demo/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let fish;
+let scalar = 0.25;
 
 function preload() {
   fish = loadImage("assets/gear.png");
@@ -19,5 +20,14 @@ function draw() {
   background(220);
   fill("red");
   // rect(mouseX, mouseY, 75, 75);
-  image(fish, mouseX, mouseY);
+  image(fish, mouseX, mouseY, scalar*fish.width, scalar*fish.height);
+}
+
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    scalar *= 1.1;
+  }
+  else {
+    scalar *= 0.9;
+  }
 }
