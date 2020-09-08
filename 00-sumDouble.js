98cb1aa868dd35a0add94800af65381d7ff3db02
Dan Schellenberg <schellenberg@gmail.com>
2020-09-08 15:11:36 -0600

    first day demos

---
+++ 00-sumDouble.js
@@ -0,0 +1,8 @@
+function sumDouble(a,b){
+  if (a === b) {
+    return a * 4;
+  }
+  else {
+    return a + b;
+  }
+}
---
+++ 00-sum_double.py
@@ -0,0 +1,9 @@
+def sum_double(a, b):
+    if a == b:
+        return a * 4
+    else:
+        return a + b
+    
+print( sum_double(1, 2) )
+print( sum_double(3, 2) )
+print( sum_double(2, 2) )
