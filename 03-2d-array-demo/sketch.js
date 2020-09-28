a39db928042b3bbbcc0272486db87f3783bd467e
Dan Schellenberg <schellenberg@gmail.com>
2020-09-28 11:44:26 -0600

    Update sketch.js

--- 03-2d-array-demo/sketch.js
+++ 03-2d-array-demo/sketch.js
@@ -9,6 +9,31 @@ function setup() {
 
 function draw() {
   background(220);
+  displayGrid();
+}
+
+function keyPressed() {
+  if (key === " ") {
+    grid = generateRandomGrid(10);
+  }
+}
+
+function displayGrid() {
+  let cellWidth = width / grid[0].length;
+  let cellHeight = height / grid.length;
+
+  for (let y=0; y<grid.length; y++) {
+    for (let x=0; x<grid[y].length; x++) {
+      if (grid[y][x] === 0) {
+        fill("black");
+      }
+      else {
+        fill("white");
+      }
+
+      rect(cellWidth*x, cellHeight*y, cellWidth, cellHeight);
+    }
+  }
 }
 
 
