8389f39607f632a8e20302d6a66afb534eb638bb
Dan Schellenberg <schellenberg@gmail.com>
2020-10-02 09:44:30 -0600

    sudoku from text file

--- 07-sudoku/sketch.js
+++ 07-sudoku/sketch.js
@@ -1,31 +1,32 @@
 // Sudoku 2d array demo
 
-let sudoku = [[5,3,0,0,7,0,0,0,0],
-              [6,0,0,1,9,5,0,0,0],
-              [0,9,8,0,0,0,0,6,0],
-              [8,0,0,0,6,0,0,0,3],
-              [4,0,0,8,0,3,0,0,1],
-              [7,0,0,0,2,0,0,0,6],
-              [0,6,0,0,0,0,2,8,0],
-              [0,0,0,4,1,9,0,0,5],
-              [0,0,0,0,8,0,0,7,9]];
-
-let initialState = 
-              [[5,3,0,0,7,0,0,0,0],
-              [6,0,0,1,9,5,0,0,0],
-              [0,9,8,0,0,0,0,6,0],
-              [8,0,0,0,6,0,0,0,3],
-              [4,0,0,8,0,3,0,0,1],
-              [7,0,0,0,2,0,0,0,6],
-              [0,6,0,0,0,0,2,8,0],
-              [0,0,0,4,1,9,0,0,5],
-              [0,0,0,0,8,0,0,7,9]];
+let sudoku;
+let initialState;
 
 const GRIDSIZE = 9;
 let cellSize;
 
+function preload() {
+  sudoku = loadStrings("assets/1.txt");
+  initialState = loadStrings("assets/1.txt");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // convert suduko into 2d array
+  for (let i=0; i<sudoku.length; i++) {
+    sudoku[i] = sudoku[i].split(",");
+    initialState[i] = initialState[i].split(",");
+  }
+
+  //loop through the whole 2d array, and turn everything to numbers
+  for (let y=0; y<GRIDSIZE; y++) {
+    for (let x=0; x<GRIDSIZE; x++) {
+      sudoku[y][x] = int(sudoku[y][x]);
+      initialState[y][x] = int(initialState[y][x]);
+    }
+  }
 
   if (width < height) {
     cellSize = width / GRIDSIZE;
