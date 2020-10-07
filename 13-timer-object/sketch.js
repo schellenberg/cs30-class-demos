9b3aba825d312c3da0d041462d9e1a7da2a63aa7
Dan Schellenberg <schellenberg@gmail.com>
2020-10-07 09:28:05 -0600

    timer demo

---
+++ 13-timer-object/sketch.js
@@ -0,0 +1,40 @@
+// Timer demo
+
+let blinkTime;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  blinkTime = new Timer(1000);
+}
+
+function draw() {
+  if (blinkTime.isDone()) {
+    background("red");
+    blinkTime.setWaitTime(2000);
+    blinkTime.reset();
+  }
+  else {
+    background("white");
+  }
+}
+
+class Timer {
+  constructor(howLongToWait) {
+    this.howLongToWait = howLongToWait;
+    this.beginTime = millis();
+    this.endTime = this.beginTime + this.howLongToWait;
+  }
+
+  isDone() {
+    return millis() >= this.endTime;
+  }
+
+  reset() {
+    this.beginTime = millis();
+    this.endTime = this.beginTime + this.howLongToWait;
+  }
+
+  setWaitTime(howLongToWait) {
+    this.howLongToWait = howLongToWait;
+  }
+}
