47a6709b4cd419927b71d2451077524bea63ea47
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-01-25 13:35:10 -0600

    Update sketch.js

--- 11-2d-grid/sketch.js
+++ 11-2d-grid/sketch.js
@@ -6,13 +6,19 @@ let grid = [[0,1,0,1,0],
             [0,1,0,1,0],
             [0,0,1,0,0]];
 
-let rows = grid.length;
-let cols = grid[0].length;
+let rows;
+let cols;
 let cellWidth;
 let cellHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  //replace hardcoded grid with empty grid
+  grid = createEmptyGrid(30, 20);
+
+  rows = grid.length;
+  cols = grid[0].length;
   cellWidth = width / cols;
   cellHeight = height / rows;
 }
@@ -32,4 +38,27 @@ function draw() {
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX / cellWidth);
+  let y = Math.floor(mouseY / cellHeight);
+
+  if (grid[y][x] === 1) {
+    grid[y][x] = 0;
+  }
+  else if (grid[y][x] === 0) {
+    grid[y][x] = 1;
+  }
+}
+
+function createEmptyGrid(cols, rows) {
+  let emptyGrid = [];
+  for (let y=0; y<rows; y++) {
+    emptyGrid.push([]);
+    for (let x=0; x<cols; x++) {
+      emptyGrid[y].push(0);
+    }
+  }
+  return emptyGrid;
 }
