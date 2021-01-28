2ac765141d1cbb32a989a18bdeb487349b04f030
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-01-28 13:38:44 -0600

    finished version

--- 15-player-in-grid/sketch.js
+++ 15-player-in-grid/sketch.js
@@ -5,6 +5,15 @@ const COLS = 20;
 let grid, cellWidth, cellHeight;
 let playerX = 0;
 let playerY = 0;
+let someMaze;
+let playerImg, wallImg, grassImg;
+
+function preload() {
+  someMaze = loadJSON("assets/myMaze.json");
+  playerImg = loadImage("assets/red-ghost.png");
+  wallImg = loadImage("assets/wall.png");
+  grassImg = loadImage("assets/grass.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -46,6 +55,9 @@ function keyPressed() {
   if (key === "w") {
     movePlayer(playerX, playerY-1, playerX, playerY, "up");
   }
+  if (key === "m") {
+    grid = someMaze;
+  }
 }
 
 function movePlayer(x, y, oldX, oldY, direction) {
@@ -72,15 +84,18 @@ function displayGrid() {
   for (let y=0; y<ROWS; y++) {
     for (let x=0; x<COLS; x++) {
       if (grid[y][x] === 0) {
-        fill("white");
+        // fill("white");
+        image(grassImg, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
       else if (grid[y][x] === 1) {
-        fill("black");
+        // fill("black");
+        image(wallImg, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
       else if (grid[y][x] === 9) {
-        fill("red");
+        // fill("red");
+        image(playerImg, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      // rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
 }
