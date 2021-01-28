b79e8d9affa74f1e59f3d8054a29e4f96bc17eb9
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-01-28 12:50:25 -0600

    player in grid

---
+++ 15-player-in-grid/sketch.js
@@ -0,0 +1,85 @@
+// Player in Grid
+
+const ROWS = 20;
+const COLS = 20;
+let grid, cellWidth, cellHeight;
+let playerX = 0;
+let playerY = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  grid = createEmptyGrid(COLS, ROWS);
+  cellWidth = width / COLS;
+  cellHeight = height / ROWS;
+  //add player to grid
+  grid[playerY][playerX] = 9;
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function keyPressed() {
+  if (key === "d") {
+    movePlayer(playerX+1, playerY, playerX, playerY, "right");
+  }
+  if (key === "a") {
+    movePlayer(playerX-1, playerY, playerX, playerY, "left");
+  }
+  if (key === "s") {
+    movePlayer(playerX, playerY+1, playerX, playerY, "down");
+  }
+  if (key === "w") {
+    movePlayer(playerX, playerY-1, playerX, playerY, "up");
+  }
+}
+
+function movePlayer(x, y, oldX, oldY, direction) {
+  if (x >= 0 && x < COLS && y >= 0 && y < ROWS) {
+    grid[y][x] = 9; //new player location
+    grid[oldY][oldX] = 0; //remove player from old spot
+
+    if (direction === "right") {
+      playerX += 1;
+    }
+    if (direction === "left") {
+      playerX -= 1;
+    }
+    if (direction === "down") {
+      playerY += 1;
+    }
+    if (direction === "up") {
+      playerY -= 1;
+    }
+  }
+}
+
+function displayGrid() {
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      else if (grid[y][x] === 9) {
+        fill("red");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
+
+function createEmptyGrid(cols, rows) {
+  let empty = [];
+  for (let y=0; y<rows; y++) {
+    empty.push([]);
+    for (let x=0; x<cols; x++) {
+      empty[y].push(0);
+    }
+  }
+  return empty;
+}
