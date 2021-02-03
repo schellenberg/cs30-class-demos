bd4c614d6f99558463888367a31bfb34b7c79d42
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-02-03 13:00:56 -0600

    updated with collisions

--- 21-ball-bounce/sketch.js
+++ 21-ball-bounce/sketch.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 // OOP Ball Bounce Demo
 
 let ballArray = [];
@@ -9,6 +10,15 @@ function setup() {
 function draw() {
   background("white");
   for (let i=0; i<ballArray.length; i++) {
+
+    //collision check
+    for (let j=0; j<ballArray.length; j++) {
+      //don't check self for collision
+      if (i !== j) {
+        ballArray[i].checkIfCollidingWith(ballArray[j]);
+      }
+    }
+
     ballArray[i].move();
     ballArray[i].display();
   }
@@ -45,6 +55,25 @@ class Ball {
     }
     if (this.y - this.radius < 0 || this.y + this.radius > height) {
       this.dy *= -1;
+    }
+  }
+
+  checkIfCollidingWith(otherBall) {
+    let sumOfRadii = this.radius + otherBall.radius;
+    let distanceBetweenCenters = dist(this.x, this.y, 
+                                      otherBall.x, otherBall.y);
+    if (sumOfRadii > distanceBetweenCenters) {
+      //temporary thing
+      // this.someColor = "red";
+      // otherBall.someColor = "red";
+
+      //react to collision
+      let tempDx = this.dx;
+      let tempDy = this.dy;
+      this.dx = otherBall.dx;
+      this.dy = otherBall.dy;
+      otherBall.dx = tempDx;
+      otherBall.dy = tempDy;
     }
   }
 }
