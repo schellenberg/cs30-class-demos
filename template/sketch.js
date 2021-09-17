59062a538f68eb0dfb05522a00ba893b6cc91910
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-09-17 10:15:37 -0600

    added template

---
+++ template/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let theWidthOfSomeRectangle = 30;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX, mouseY, theWidthOfSomeRectangle, 50);
+  ellipse(100, 200, 10, 10);
+}
