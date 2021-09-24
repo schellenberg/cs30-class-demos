0b1a31d21d07306785e144e8f3f89da0032d216e
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-09-24 12:09:49 -0600

    added traffic light

---
+++ traffic-light/sketch.js
@@ -0,0 +1,12 @@
+// First Demo
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  fill("blue");
+  ellipse(mouseX, mouseY, 100, 50);
+}
