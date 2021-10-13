3f3693f9346bd6a2060dfccadb90d0f6c82bb9fb
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-10-13 12:11:24 -0600

    grid neighbours

---
+++ 09-grid-neighbours/sketch.js
@@ -0,0 +1,80 @@
+// Grid Demo
+
+let gridSize = 30;
+let grid;
+let clickSound;
+
+function preload() {
+  clickSound = loadSound("assets/click.wav");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = createRandom2DArray(gridSize, gridSize);
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function mousePressed() {
+  clickSound.play();
+
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+
+  let cellX = Math.floor(mouseX/cellWidth);
+  let cellY = Math.floor(mouseY/cellHeight);
+
+  if (grid[cellY][cellX] === 1) {
+    grid[cellY][cellX] = 0;
+  }
+  else if (grid[cellY][cellX] === 0) {
+    grid[cellY][cellX] = 1;
+  }
+}
+
+function displayGrid() {
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+
+  for (let y=0; y<gridSize; y++) {
+    for (let x=0; x<gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
+
+function createEmpty2DArray(rows, cols) {
+  let grid = [];
+  for (let y=0; y<rows; y++) {
+    grid.push([]);
+    for (let x=0; x<cols; x++) {
+      grid[y].push(0);
+    }
+  }
+  return grid;
+}
+
+function createRandom2DArray(rows, cols) {
+  let grid = [];
+  for (let y=0; y<rows; y++) {
+    grid.push([]);
+    for (let x=0; x<cols; x++) {
+      if (random(100) < 50) {
+        grid[y].push(0);
+      }
+      else {
+        grid[y].push(1);
+      }
+    }
+  }
+  return grid;
+}
