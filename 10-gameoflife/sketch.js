979f3165461b73ca309cf28b5f47bf5c6c17726b
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-10-14 12:10:58 -0600

    game of life started

---
+++ 10-gameoflife/sketch.js
@@ -0,0 +1,19 @@
+// Game of Life
+
+let grid;
+let gridSize = 10;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = createEmpty2DArray(gridSize, gridSize);
+}
+
+function draw() {
+  background(220);
+
+
+}
+
+function createEmpty2DArray(rows, cols) {
+  
+}
