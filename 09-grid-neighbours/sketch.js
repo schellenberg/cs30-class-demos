8cfecb6f5cbc9a18abe5e8640369ba6e9a9ff647
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-10-14 11:39:01 -0600

    neighbours

--- 09-grid-neighbours/sketch.js
+++ 09-grid-neighbours/sketch.js
@@ -18,6 +18,18 @@ function draw() {
   displayGrid();
 }
 
+function keyPressed() {
+  if (key === "e") {
+    grid = createEmpty2DArray(gridSize, gridSize);
+  }
+  if (key === "b") {
+    grid = createEmpty2DArray(gridSize, gridSize, 1);
+  }
+  if (key === "r") {
+    grid = createRandom2DArray(gridSize, gridSize);
+  }
+}
+
 function mousePressed() {
   clickSound.play();
 
@@ -27,11 +39,21 @@ function mousePressed() {
   let cellX = Math.floor(mouseX/cellWidth);
   let cellY = Math.floor(mouseY/cellHeight);
 
-  if (grid[cellY][cellX] === 1) {
-    grid[cellY][cellX] = 0;
-  }
-  else if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
+  swap(cellX, cellY);
+  swap(cellX+1, cellY);
+  swap(cellX-1, cellY);
+  swap(cellX, cellY+1);
+  swap(cellX, cellY-1);
+}
+
+function swap(x, y) {
+  if (x >= 0 && x < gridSize && y >=0 && y < gridSize) {
+    if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+    else if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
   }
 }
 
@@ -52,12 +74,12 @@ function displayGrid() {
   }
 }
 
-function createEmpty2DArray(rows, cols) {
+function createEmpty2DArray(rows, cols, numToFill = 0) {
   let grid = [];
   for (let y=0; y<rows; y++) {
     grid.push([]);
     for (let x=0; x<cols; x++) {
-      grid[y].push(0);
+      grid[y].push(numToFill);
     }
   }
   return grid;
