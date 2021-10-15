0e289acfba2393d4d0db5fc493eb45824a6fbdbe
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-10-15 11:21:57 -0600

    Update sketch.js

--- 10-gameoflife/sketch.js
+++ 10-gameoflife/sketch.js
@@ -2,18 +2,41 @@
 
 let grid;
 let gridSize = 10;
+let cellWidth, cellHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = createEmpty2DArray(gridSize, gridSize);
+  cellWidth = width/gridSize;
+  cellHeight = height/gridSize;
 }
 
 function draw() {
   background(220);
+  displayGrid();
+}
 
-
+function displayGrid() {
+  for (let y=0; y<gridSize; y++) {
+    for (let x=0; x<gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
 }
 
 function createEmpty2DArray(rows, cols) {
-  
+  let board = [];
+  for (let y=0; y<rows; y++) {
+    board.push([]);
+    for (let x=0; x<cols; x++) {
+      board[y].push(0);
+    }
+  }
+  return board;
 }
