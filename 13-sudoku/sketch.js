de8d9d679db3d53dffd993241682099513f794d5
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-10-21 12:07:49 -0600

    centered

--- 13-sudoku/sketch.js
+++ 13-sudoku/sketch.js
@@ -12,12 +12,50 @@ let initialGrid = [
   [3, 0, 0, 4, 0, 0, 5, 0, 8],
 ];
 
+let gridDimensions = 9;
+let cellSize;
+let grid;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth*0.8, windowWidth*0.8);
+  }
+  else {
+    createCanvas(windowHeight*0.8, windowHeight*0.8);
+  }
+  cellSize = (width-2)/gridDimensions;
+  grid = initialGrid;
 }
 
 function draw() {
   background(220);
+  displayGrid();
+}
 
-  rect(mouseX, mouseY, 100, 50);
+function displayGrid() {
+  for (let y=0; y<gridDimensions; y++) {
+    for (let x=0; x<gridDimensions; x++) {
+      //draw the square
+      fill("white");
+      strokeWeight(1);
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+     
+      //show the number
+      if (grid[y][x] !== 0) {
+        fill("black");
+        textSize(cellSize*0.75);
+        textAlign(CENTER, CENTER);
+        text(grid[y][x], x*cellSize + cellSize/2, y*cellSize + cellSize/2);
+      }
+    }
+  }
+  drawCageLines();
+}
+
+function drawCageLines() {
+  strokeWeight(4);
+  for (let location = 0; location <= 9; location += 3) {
+    line(0, cellSize*location, width, cellSize*location);
+    line(cellSize*location, 0, cellSize*location, height);
+  }
 }
