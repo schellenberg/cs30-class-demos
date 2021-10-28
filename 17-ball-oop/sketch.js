52c6f1ca1bcb479b5727bda928c80e7771741654
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-10-28 10:35:22 -0600

    Update sketch.js

--- 17-ball-oop/sketch.js
+++ 17-ball-oop/sketch.js
@@ -1,24 +1,36 @@
 // Ball OOP Demo
 
-let theBall;
+let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  theBall = new Ball();
+  for (let i=0; i<10; i++) {
+    let x = random(100, width-100);
+    let y = random(100, height-100);
+    let theBall = new Ball(x, y);
+    ballArray.push(theBall);
+  }
 }
 
 function draw() {
   background(220);
-  
-  theBall.move();
-  theBall.display();
+
+  // for (let theBall of ballArray) {
+  //   theBall.move();
+  //   theBall.display();
+  // }
+
+  for (let i=0; i<ballArray.length; i++) {
+    ballArray[i].move();
+    ballArray[i].display();
+  }
 }
 
 class Ball {
-  constructor() {
+  constructor(x, y) {
     this.radius = random(20, 50);
-    this.x = random(this.radius, width-this.radius);
-    this.y = random(this.radius, height-this.radius);
+    this.x = x;
+    this.y = y;
     this.dx = random(-5, 5);
     this.dy = random(-5, 5);
     this.theColor = color(random(255), random(255), random(255), random(255));
@@ -34,6 +46,12 @@ class Ball {
     this.x += this.dx;
     this.y += this.dy;
 
-
+    //check if need to bounce
+    if (this.x + this.radius >= width || this.x - this.radius <= 0) {
+      this.dx *= -1;
+    }
+    if (this.y + this.radius >= height || this.y - this.radius <= 0) {
+      this.dy *= -1;
+    }
   }
 }
