eaa4d0193c0c52cc0f7c713a99efdb12f5f5b479
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-10-28 10:53:19 -0600

    added image

--- 17-ball-oop/sketch.js
+++ 17-ball-oop/sketch.js
@@ -1,13 +1,18 @@
 // Ball OOP Demo
 
 let ballArray = [];
+let jeffImg;
+
+function preload() {
+  jeffImg = loadImage("assets/jeff.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i=0; i<10; i++) {
     let x = random(100, width-100);
     let y = random(100, height-100);
-    let theBall = new Ball(x, y);
+    let theBall = new Ball(x, y, jeffImg);
     ballArray.push(theBall);
   }
 }
@@ -26,20 +31,39 @@ function draw() {
   }
 }
 
+function mousePressed() {
+  let theBall = new Ball(mouseX, mouseY, jeffImg);
+  ballArray.push(theBall);
+}
+
 class Ball {
-  constructor(x, y) {
+  constructor(x, y, theImage) {
     this.radius = random(20, 50);
     this.x = x;
     this.y = y;
     this.dx = random(-5, 5);
     this.dy = random(-5, 5);
     this.theColor = color(random(255), random(255), random(255), random(255));
+    this.theImage = theImage;
+    let choice = random(100);
+    if (choice < 50) {
+      this.toDisplay = "circle";
+    }
+    else {
+      this.toDisplay = "image";
+    }
   }
 
   display() {
-    noStroke();
-    fill(this.theColor);
-    circle(this.x, this.y, this.radius*2);
+    if (this.toDisplay === "circle") {
+      noStroke();
+      fill(this.theColor);
+      circle(this.x, this.y, this.radius*2);
+    }
+    else if (this.toDisplay === "image") {
+      imageMode(CENTER);
+      image(this.theImage, this.x, this.y, this.radius*2, this.radius*2);
+    }
   }
 
   move() {
