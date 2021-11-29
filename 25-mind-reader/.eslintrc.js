66cf07d27114e04d3babdfc74df40c8e37efdf4a
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-11-29 11:07:30 -0600

    Update .eslintrc.js

--- 25-mind-reader/.eslintrc.js
+++ 25-mind-reader/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         "es6": true
     },
     "extends": "eslint:recommended",
+    "ecmaFeatures": {
       "blockBindings": true,
       "defaultParams": true,
       "forOf": true
