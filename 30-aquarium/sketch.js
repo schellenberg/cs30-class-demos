d11e0252f60b1f15babc9f0805acd8995faf25c9
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2021-12-16 11:48:57 -0600

    octopus

--- 30-aquarium/sketch.js
+++ 30-aquarium/sketch.js
@@ -1,19 +1,25 @@
 // Aquarium
 
 let fish;
+let octopus;
 let clownfishImg;
+let octopusImg;
 
 function preload() {
   clownfishImg = loadImage("assets/clownfish.png");
+  octopusImg = loadImage("assets/octopus.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fish = new Clownfish(random(width), random(height), 30, clownfishImg);
+  octopus = new Octopus(100, 200, 100, octopusImg);
+  fish = new Clownfish(random(width), random(100, height-100), 30, clownfishImg);
 }
 
 function draw() {
   background(220);
+  octopus.update();
+  octopus.display();
   fish.update();
   fish.display();
 }
@@ -39,9 +45,42 @@ class Clownfish extends Creature {
   constructor(x, y, size, theImage) {
     super(x, y, size);
     this.myImage = theImage;
+    this.yTime = 1000;
+    this.speed = random(1, 2);
+  }
+
+  update() {
+    //Logan's movement
+    // this.x += 2;
+    // this.y = noise(this.yTime)*height;
+    // this.yTime += random(0.001, 0.005);
+
+    //Declan's Movement
+    // this.x += this.speed;
+    // this.y += sin(this.x / 100);
+
+    //Daniel's Movement
+    this.x += random(0.1, 3);
+    this.y = noise(this.x/300, second()/600) * height;
+
+    // //teleport from right to left
+    if (this.x > width) {
+      this.x = 0;
+    }
   }
 
   display() {
     image(this.myImage, this.x, this.y, this.size, this.size);
+  }
+}
+
+class Octopus extends Creature {
+  constructor(x, y, size, someImage) {
+    super(x, y, size);
+    this.image = someImage;
+  }
+
+  display() {
+    image(this.image, this.x, this.y, this.size, this.size);
   }
 }
