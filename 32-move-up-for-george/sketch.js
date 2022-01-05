a0d3ff6f3bbea544b4610bd0bfccf420d9c524db
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-01-05 12:17:16 -0600

    for george

---
+++ 32-move-up-for-george/sketch.js
@@ -0,0 +1,27 @@
+let grid = [[0,0,0],
+            [2,0,2],
+            [4,0,2]];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = moveUp(grid);
+}
+
+function draw() {
+  background(220);
+}
+
+function moveUp(grid) {
+  for (let y=1; y<3; y++) {
+    for (let x=0; x<3; x++) {
+      if (grid[y][x] !== 0) {
+        let thisY = y;
+        let thisX = x;
+        while (grid[thisY-1][thisX] === 0) {
+          // swapWithAbove
+          //change thisY
+        }
+      }
+    }
+  }
+}
