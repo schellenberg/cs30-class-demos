3f064eeb2db28d96a9e721efada25d196594f510
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-09-16 13:07:36 -0600

    setting up vs code demo

---
+++ 04-chessboard/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background("black");
+  fill("white");
+  square(mouseX, mouseY, 50);
+}
