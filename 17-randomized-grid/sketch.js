d997b2b75a83305e8beb5caffce19d29373602f4
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-10-26 12:03:22 -0600

    Update sketch.js

--- 17-randomized-grid/sketch.js
+++ 17-randomized-grid/sketch.js
@@ -2,17 +2,17 @@
 // Schellenberg
 // Oct 26, 2022
 
-let rows = 10;
-let cols = 10;
+const ROWS = 40;
+const COLS = 40;
 let grid;
 let cellWidth;
 let cellHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cellWidth = width/cols;
-  cellHeight = height/rows;
-  grid = createRandom2dArray(cols, rows);
+  cellWidth = width/COLS;
+  cellHeight = height/ROWS;
+  grid = createRandom2dArray(COLS, ROWS);
 }
 
 function draw() {
@@ -20,9 +20,21 @@ function draw() {
   displayGrid(grid);
 }
 
+function mousePressed() {
+  let xPos = Math.floor(mouseX/cellWidth);
+  let yPos = Math.floor(mouseY/cellHeight);
+
+  if (grid[yPos][xPos] === 0) {
+    grid[yPos][xPos] = 1;
+  }
+  else if (grid[yPos][xPos] === 1) {
+    grid[yPos][xPos] = 0;
+  }
+}
+
 function displayGrid(grid) {
-  for (let y=0; y<rows; y++) {
-    for (let x=0; x<cols; x++) {
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
       if (grid[y][x] === 0) {
         fill("white");
       }
@@ -34,22 +46,22 @@ function displayGrid(grid) {
   }
 }
 
-function create2dArray(cols, rows) {
+function create2dArray(COLS, ROWS) {
   let emptyArray = [];
-  for (let y=0; y<rows; y++) {
+  for (let y=0; y<ROWS; y++) {
     emptyArray.push([]);
-    for (let x=0; x<cols; x++) {
+    for (let x=0; x<COLS; x++) {
       emptyArray[y].push(0);
     }
   }
   return emptyArray;
 }
 
-function createRandom2dArray(cols, rows) {
+function createRandom2dArray(COLS, ROWS) {
   let emptyArray = [];
-  for (let y=0; y<rows; y++) {
+  for (let y=0; y<ROWS; y++) {
     emptyArray.push([]);
-    for (let x=0; x<cols; x++) {
+    for (let x=0; x<COLS; x++) {
       if (random(100) < 50) {
         emptyArray[y].push(0);
       }
