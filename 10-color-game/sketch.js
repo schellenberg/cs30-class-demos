58ee7f17202a5bdb862020a49d49b60c760532dd
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-03-21 15:14:42 -0600

    Update sketch.js

--- 10-color-game/sketch.js
+++ 10-color-game/sketch.js
@@ -25,6 +25,29 @@ function draw() {
   displayGrid(grid);
 }
 
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  toggleCell(x, y);
+  toggleCell(x+1, y); //E
+  toggleCell(x-1, y); //W
+  toggleCell(x, y-1); //N
+  toggleCell(x, y+1); //S
+}
+
+function toggleCell(x, y) {
+  //sanity check
+  if (x >= 0 && x < COLS && y >= 0 && y < ROWS) {
+    if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+  }
+}
+
 function displayGrid(grid) {
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x < COLS; x++) {
