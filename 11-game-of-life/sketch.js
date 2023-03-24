b4d36a3b4254e4e42f3765bf401988cf1b084315
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-03-24 14:32:37 -0600

    updated game of life

--- 11-game-of-life/sketch.js
+++ 11-game-of-life/sketch.js
@@ -2,11 +2,16 @@
 // Dan Schellenberg
 // March 21, 2023
 
-const ROWS = 10;
-const COLS = 10;
+const ROWS = 40;
+const COLS = 40;
 let grid;
 let cellSize;
 let autoUpdate = true;
+let gosperGun;
+
+function preload() {
+  gosperGun = loadJSON("gosper.json");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -41,6 +46,10 @@ function keyTyped() {
   }
   else if (key === "a") {
     autoUpdate = !autoUpdate;
+  }
+  else if (key === "g") {
+    //assumes a 40x40 grid
+    grid = gosperGun;
   }
 }
 
