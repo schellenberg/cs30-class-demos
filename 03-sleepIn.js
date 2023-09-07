7a85045e47f9e5e498b0b03c932314515da28c59
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-09-07 11:30:11 -0600

    js versions

---
+++ 03-sleepIn.js
@@ -0,0 +1,29 @@
+function sleepIn(weekday,vacation) {
+//     #Nohl
+//      if (vacation === true) {
+//          return true;
+//      }
+//      else if (weekday === false) {
+//          return true;
+//      }
+//      else {
+//          return false;
+//      }
+  
+  
+  //     #Gabe's
+//      if (!weekday || vacation) {
+//          return true;
+//      }
+//      else {
+//          return false;
+//      }
+  
+
+    
+//     # Robert
+     return !weekday || vacation;
+    
+
+     
+}
