3d09fb843ef86e21d8497216e572e0cb43465470
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2023-10-04 11:35:47 -0600

    added ball demo

---
+++ 01-ball/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, 50);
+}
