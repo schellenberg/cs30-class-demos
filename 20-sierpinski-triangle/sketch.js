281fb7b44156c77341de362da475c916774f881e
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2024-05-29 13:56:00 -0600

    Update sketch.js

--- 20-sierpinski-triangle/sketch.js
+++ 20-sierpinski-triangle/sketch.js
@@ -7,16 +7,26 @@ let initialTriangle = [
   {x: 1100, y: 780}
 ];
 
+let theDepth = 0;
+let theColors = ["black", "red", "blue", "yellow", "brown", "purple", "orange", "green"];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  sierpinski(initialTriangle, 4);
+  sierpinski(initialTriangle, theDepth);
+}
+
+function mousePressed() {
+  if (theDepth < 7) {
+    theDepth++;
+  }
 }
 
 function sierpinski(points, depth) {
+  fill(theColors[depth]);
   triangle(points[0].x, points[0].y, 
            points[1].x, points[1].y,
            points[2].x, points[2].y,
@@ -29,6 +39,17 @@ function sierpinski(points, depth) {
                 points[1]],
                 depth - 1);
 
+    //draw the upper triangle
+    sierpinski([midpoint(points[0], points[1]),
+                midpoint(points[0], points[2]),
+                points[0]],
+                depth - 1);
+
+    //draw the lower right triangle
+    sierpinski([midpoint(points[0], points[2]),
+                midpoint(points[1], points[2]),
+                points[2]],
+                depth - 1);
   }
 }
 
