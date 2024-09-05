eb253be22aaee693921d9f349a07b74d8f27971b
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-09-05 14:07:09 -0600

    Create 05-parrotTrouble.js

---
+++ Period 4/codingjs/05-parrotTrouble.js
@@ -0,0 +1,20 @@
+function parrotTrouble(talking, hour) {
+  return (hour < 7 || hour > 20) && talking;
+  
+//   if ((hour < 7 || hour > 20) && talking) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+  
+//   if (hour < 7 && talking) {
+//     return true;
+//   }
+//   else if (hour > 20 && talking) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+}
