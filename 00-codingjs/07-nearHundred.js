5d44df1af55a0b5423a63dea75c1098bf54ba71c
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-02-04 13:30:11 -0600

    codingjs

---
+++ 00-codingjs/07-nearHundred.js
@@ -0,0 +1,12 @@
+function nearHundred(n) {
+  return Math.abs(100-n) <= 10 || Math.abs(200-n) <= 10;
+  
+//   return n >= 90 && n <= 110 || n >= 190 && n <= 210;
+  
+//   if (n >= 90 && n <= 110 || n >= 190 && n <= 210) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+}
