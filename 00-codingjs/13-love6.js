18306f0d83ea9249fdae976ae3b4def59dd4fad9
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-02-06 13:36:47 -0600

    Create 13-love6.js

---
+++ 00-codingjs/13-love6.js
@@ -0,0 +1,11 @@
+function love6(a, b) {
+  
+  return a === 6 || b === 6 || a + b === 6 || Math.abs(a - b) === 6;
+  
+//   if (a === 6 || b === 6 || a + b === 6 || Math.abs(a - b) === 6) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+}
