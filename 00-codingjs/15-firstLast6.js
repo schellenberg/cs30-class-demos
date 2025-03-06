7ee622fbc90ee5b3433ffe2d20129326af1b1608
schellenberg <227656+schellenberg@users.noreply.github.com>
2025-03-06 13:28:27 -0600

    array problems

---
+++ 00-codingjs/15-firstLast6.js
@@ -0,0 +1,10 @@
+function firstLast6(nums) {
+  return nums[0] === 6 || nums[nums.length-1] === 6;
+  
+//   if (nums[0] === 6 || nums[nums.length-1] === 6) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+}
---
+++ 00-codingjs/16-sameFirstLast.js
@@ -0,0 +1,17 @@
+function sameFirstLast(nums) {
+  return nums.length >= 1 && 
+         nums[0] === nums[nums.length-1];
+  
+//   if (nums.length >= 1) {
+//     if (nums[0] === nums[nums.length-1]) {
+//       return true;
+//     }
+//     else {
+//       return false;
+//     }
+//   }
+//   else {
+//     return false;
+//   }
+  
+}
