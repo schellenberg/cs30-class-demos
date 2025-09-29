2f13016b910f4b69b2cbffe40b6f30c71a4c0306
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-09-29 09:12:42 -0600

    demoed putting in assignment

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,12 +1,48 @@
-// Interactive Scene
+// Interactive Scene Project
+// Dan Schellenberg
 
+
+let cellSize;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  cellSize = width/8;
 }
 
 function draw() {
   background(220);
+  showChessboard();
+}
 
-  circle(mouseX, mouseY, 50);
+function windowResized() {
+  if (windowWidth > windowHeight) {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+  else {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  cellSize = width/8;
+}
+
+function showChessboard() {
+  let isWhite = true;
+  
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      if (isWhite) {
+        fill("white");
+      }
+      else {
+        fill("black");
+      }
+      square(x*cellSize, y*cellSize, cellSize);
+      isWhite = !isWhite;
+    }
+    isWhite = !isWhite;
+  }
 }
