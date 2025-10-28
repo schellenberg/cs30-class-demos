9e3e58fa18aeddfa641a784ac543228261e9c37d
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2025-10-28 09:27:00 -0600

    Update sketch.js

--- neighbours/sketch.js
+++ neighbours/sketch.js
@@ -17,9 +17,38 @@ function draw() {
   displayGrid();
 }
 
+function mousePressed() {
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+
+  //self
+  toggleCell(x ,y);
+
+  //neighbours
+  toggleCell(x + 1, y);
+  toggleCell(x - 1, y);
+  toggleCell(x, y - 1);
+  toggleCell(x, y + 1);
+}
+
+function toggleCell(x, y) {
+  //make sure the cell you're toggling actually exists!
+  if (x >= 0 && x < cols && y >= 0 && y < rows) {
+    if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+  }
+}
+
 function keyPressed() {
   if (key === "r") {
     grid = generateRandomGrid(cols, rows);
+  }
+  else if (key === "e") {
+    grid = generateEmptyGrid(cols, rows);
   }
 }
 
@@ -49,6 +78,17 @@ function generateRandomGrid(cols, rows) {
       else {
         newGrid[y].push(1);
       }
+    }
+  }
+  return newGrid;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
     }
   }
   return newGrid;
