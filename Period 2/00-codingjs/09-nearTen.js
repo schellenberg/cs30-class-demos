0b46fdd1356888a785c3a6a7be7b54fee0aa4a4d
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-02-04 10:19:22 -0600

    Create 09-nearTen.js

---
+++ Period 2/00-codingjs/09-nearTen.js
@@ -0,0 +1,11 @@
+function nearTen(num) {
+//   let remainder = num % 10;
+//   if (remainder >= 8 || remainder <= 2) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+  
+  return num % 10 >= 8 || num % 10 <= 2;
+}
