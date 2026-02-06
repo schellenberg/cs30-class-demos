e8f4211204373c66e7196eadbfd0292e640bf7d9
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-02-06 14:32:00 -0600

    codingjs

---
+++ Period 5/00-codingjs/15-in1To10.js
@@ -0,0 +1,14 @@
+function in1To10(n, outsideMode) {
+//   if (outsideMode && (n <= 1 || n >= 10)) {
+//     return true;
+//   }
+//   else if (!outsideMode && n >= 1 && n <= 10) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+  
+  return (outsideMode && (n <= 1 || n >= 10)) ||
+         (!outsideMode && n >= 1 && n <= 10);
+}
