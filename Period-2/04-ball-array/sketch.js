0f074ae378d53820a59d0afce20321fc9c5afec4
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-03-04 10:46:55 -0600

    updated ball array

--- Period-2/04-ball-array/sketch.js
+++ Period-2/04-ball-array/sketch.js
@@ -4,6 +4,7 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
@@ -14,22 +15,40 @@ function draw() {
     ball.x += ball.dx;
     ball.y += ball.dy;
 
+    //teleport if needed
+    if (ball.x - ball.radius > width) {
+      ball.x = -ball.radius;
+    }
+    if (ball.x + ball.radius < 0) {
+      ball.x = width + ball.radius;
+    }
+    if (ball.y - ball.radius > height) {
+      ball.y = -ball.radius;
+    }
+    if (ball.y + ball.radius < 0) {
+      ball.y = height + ball.radius;
+    }
+
     //display
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius*2);
   }
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let someBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 30),
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255),
   };
   ballArray.push(someBall);
 }
