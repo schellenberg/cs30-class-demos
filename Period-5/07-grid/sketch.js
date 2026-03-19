d759654631a4c90ab8af1c000c9781f0e8ee70f3
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-03-19 10:24:54 -0600

    Update sketch.js

--- Period-5/07-grid/sketch.js
+++ Period-5/07-grid/sketch.js
@@ -1,11 +1,16 @@
 // 2D Grid Demo
 //learning 2d arrays
 
-let theGrid = [[1, 0, 1, 0],
-               [1, 0, 1, 0],
-               [0, 1, 0, 1],
-               [0, 0, 1, 1]];
-const SQUARE_DIMENSION = theGrid.length;
+//use this if hard coded grid
+// let theGrid = [[1, 0, 1, 0],
+//                [1, 0, 1, 0],
+//                [0, 1, 0, 1],
+//                [0, 0, 1, 1]];
+// const SQUARE_DIMENSION = theGrid.length;
+
+//use this to randomize grid
+let theGrid;
+const SQUARE_DIMENSION = 10;
 let cellSize;
 
 function setup() {
@@ -16,11 +21,29 @@ function setup() {
   else {
     cellSize = width/SQUARE_DIMENSION;
   }
+  theGrid = generateRandomGrid(SQUARE_DIMENSION, SQUARE_DIMENSION);
 }
 
 function draw() {
   background(220);
   showGrid();
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  // console.log(x, y);
+  toggleCell(x, y);
+}
+
+function toggleCell(x, y) {
+  if (theGrid[y][x] === 1) {
+    theGrid[y][x] = 0;
+  }
+  else if (theGrid[y][x] === 0) {
+    theGrid[y][x] = 1;
+  }
 }
 
 function showGrid() {
@@ -35,4 +58,20 @@ function showGrid() {
       square(x * cellSize, y * cellSize, cellSize);
     }
   }
+}
+
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(0);
+      }
+      else {
+        newGrid[y].push(1);
+      }
+    }
+  }
+  return newGrid;
 }
