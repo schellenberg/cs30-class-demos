800a37d7c9f1095d74ac2edce0ef5b76096355c9
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-03-20 15:09:41 -0600

    Update sketch.js

--- Period-5/08-rect-grid/sketch.js
+++ Period-5/08-rect-grid/sketch.js
@@ -1,6 +1,6 @@
 // 2d rectangular grid demo
 
-const CELL_SIZE = 80;
+const CELL_SIZE = 100;
 let rows;
 let cols;
 let grid;
@@ -17,8 +17,53 @@ function draw() {
   displayGrid();
 }
 
+function mousePressed() {
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+
+  //self
+  toggleCell(x, y);
+
+  //neighbours
+  toggleCell(x - 1, y);
+  toggleCell(x + 1, y);
+  toggleCell(x, y - 1);
+  toggleCell(x, y + 1);
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(cols, rows);
+  }
+  else if (key === "e") {
+    grid = generateEmptyGrid(cols, rows);
+  }
+}
+
+function toggleCell(x, y) {
+  //make sure the cell you're toggling is in the grid
+  if (x >= 0 && x < cols && y >= 0 && y < rows) {
+    if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+  }
+}
+
 function displayGrid() {
-  
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
+  }
 }
 
 function generateRandomGrid(cols, rows) {
@@ -32,6 +77,17 @@ function generateRandomGrid(cols, rows) {
       else {
         newGrid[y].push(1);
       }
+    }
+  }
+  return newGrid;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
     }
   }
   return newGrid;
