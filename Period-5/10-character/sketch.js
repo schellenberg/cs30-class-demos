baaa14cef5c16bb676d32c3dac13365a0f45936f
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-03-25 14:53:58 -0600

    character in grid

---
+++ Period-5/10-character/sketch.js
@@ -0,0 +1,132 @@
+// Character in Grid Demo
+
+const CELL_SIZE = 100;
+const OPEN_TILE = 0;
+const IMPASSIBLE = 1;
+const PLAYER = 9;
+let rows;
+let cols;
+let grid;
+let thePlayer = {
+  x: 0,
+  y: 0,
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rows = Math.floor(height/CELL_SIZE);
+  cols = Math.floor(width/CELL_SIZE);
+  grid = generateRandomGrid(cols, rows);
+  //put character in grid
+  grid[thePlayer.y][thePlayer.x] = PLAYER;
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+
+  //self
+  toggleCell(x, y);
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(cols, rows);
+    grid[thePlayer.y][thePlayer.x] = PLAYER;
+  }
+  else if (key === "e") {
+    grid = generateEmptyGrid(cols, rows);
+    grid[thePlayer.y][thePlayer.x] = PLAYER;
+  }
+  else if (key === "s") {
+    movePlayer(thePlayer.x, thePlayer.y + 1);
+  }
+  else if (key === "w") {
+    movePlayer(thePlayer.x, thePlayer.y - 1);
+  }
+  else if (key === "d") {
+    movePlayer(thePlayer.x + 1, thePlayer.y);
+  }
+  else if (key === "a") {
+    movePlayer(thePlayer.x - 1, thePlayer.y);
+  }
+}
+
+function movePlayer(x, y) {
+  if (x >= 0 && x < cols && y >= 0 && y < rows && grid[y][x] === OPEN_TILE) {
+    //keep track of where the player was
+    let oldX = thePlayer.x;
+    let oldY = thePlayer.y;
+
+    //moving the player
+    thePlayer.x = x;
+    thePlayer.y = y;
+  
+    //putting the player in grid
+    grid[thePlayer.y][thePlayer.x] = PLAYER;
+
+    //reset the old player spot to be an open tile
+    grid[oldY][oldX] = OPEN_TILE;
+  }
+}
+
+function toggleCell(x, y) {
+  //make sure the cell you're toggling is in the grid
+  if (x >= 0 && x < cols && y >= 0 && y < rows) {
+    if (grid[y][x] === OPEN_TILE) {
+      grid[y][x] = IMPASSIBLE;
+    }
+    else if (grid[y][x] === IMPASSIBLE) {
+      grid[y][x] = OPEN_TILE;
+    }
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === OPEN_TILE) {
+        fill("white");
+      }
+      else if (grid[y][x] === IMPASSIBLE) {
+        fill("black");
+      }
+      else if (grid[y][x] === PLAYER) {
+        fill("red");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(OPEN_TILE);
+      }
+      else {
+        newGrid[y].push(IMPASSIBLE);
+      }
+    }
+  }
+  return newGrid;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(OPEN_TILE);
+    }
+  }
+  return newGrid;
+}
