27dac99efa5a93e847ed925fbeae153914d9d591
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-09-03 15:23:59 -0600

    codingjs

---
+++ 00-codingjs/03-sumDouble.js
@@ -0,0 +1,18 @@
+function sumDouble(a, b) {
+//       #James
+//   if (a !== b) {
+//     return a + b;
+//   }
+//   else {
+//     return 2 * (a + b);
+//   }
+
+//     #Luca
+  if (a === b) {
+    return 2 * (a + b);
+  }
+  else {
+    return a + b;
+  }
+
+}
---
+++ 00-codingjs/04-sleepIn.js
@@ -0,0 +1,31 @@
+function sleepIn(weekday, vacation) {
+//       #Ryley
+//   if (weekday === false) {
+//     return true;
+//   }
+//   else if (vacation === true) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+    
+//     #Theo
+//   if (weekday === false || vacation === true) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+
+//     #James and Luca
+//   if (!weekday || vacation) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+    
+//     #Asher
+    return !weekday || vacation;
+}
