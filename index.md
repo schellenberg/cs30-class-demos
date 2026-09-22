dfa81eb5de9380ca4f3a3533e6c4c30c72f39568
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2026-09-22 15:15:34 -0600

    added scene template

--- index.md
+++ index.md
@@ -6,4 +6,4 @@
 - [Square Around Edge of Screen](03-square)
 
 ## Projects
-- first assignment will go here
+- [Interactive Scene](scene)
---
+++ scene/sketch.js
@@ -0,0 +1,15 @@
+// Interactive Scene
+// Dan Schellenberg
+// Sept 22, 2026
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+async function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
